c91bc19471290e0d6f0b473b9b8c8fc868cfc701
abdurrahman <abdurrahman126amir@gmail.com>
2024-11-18 14:47:31 +0500

    backend integrated with auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,24 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import {RouterProvider,createBrowserRouter,createRoutesFromElements,Route,Link,Outlet} from "react-router-dom";
 import Home from './features/home/Home'
-import Login from './features/login/Login'
+import Login,{action as loginAction} from './features/login/Login'
 import Navbar from './features/nav/Navbar';
 import EventPage,{loader as eventLoader} from './features/event/EventPage';
 import Booking,{loader as bookingLoader,action as bookingAction} from './features/event/Booking';
-import Register from './features/login/Register';
+import Register,{action as registerAction} from './features/login/Register';
 import MemberHome from './features/home/MemberHome';
+import AdminHome from './features/admin/AdminHome';
 const router=createBrowserRouter(createRoutesFromElements(
   <>
   <Route path="/" element={<Navbar/>}>
     <Route index element={<Home/>} />
-    <Route path="login" element={<Login/>}/>
+    <Route path="login" element={<Login/>} action={loginAction}/>
     <Route path="events" element={<EventPage/>} loader={eventLoader}/>
-    <Route path="register" element={<Register/>} />
-  
+    <Route path="register" element={<Register/>} action={registerAction}/>
   </Route>
   <Route path="/events/:id" element={<Booking/>} loader={bookingLoader} action={bookingAction}/>
   <Route path="/members" element={<MemberHome/>} />
+  <Route path="/admin" element={<AdminHome/>} />
   
   </>)
 )
--- client/src/features/login/Register.jsx
+++ client/src/features/login/Register.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
-import { Form } from 'react-router-dom'
+import { Form ,redirect} from 'react-router-dom'
+
+
+export const action = async ({ request }) => {
+  const formData = new URLSearchParams(await request.formData());
+  
+  const response = await fetch('http://127.0.0.1:5001/api/register', {
+    method: 'POST',
+    body: formData,
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+      throw redirect('/members')
+    return  {message:data.message}
+  } else {
+    console.log(data.message)
+    return { error: data.error }
+  }
+};
 
 const Register = () => {
   return (
@@ -7,12 +27,12 @@ const Register = () => {
     <div className='bg-white lg:w-[35%] w-[80%] h-[60%] rounded-2xl flex flex-col items-center justify-evenly animate-open py-4'> 
     <h1 className='text-black lg:text-4xl text-2xl font-bold'>Member Induction</h1>
   
-    <Form className='w-[90%] flex flex-col justify-evenly items-center gap-4'>
-     <input name="email" placeholder='Full Name' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
-     <input name="email" placeholder='Batch e.g:22k' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
+    <Form method="post" className='w-[90%] flex flex-col justify-evenly items-center gap-4'>
+     <input name="name" placeholder='Full Name' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
+     <input name="batch" placeholder='Batch e.g:22k' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
      
-     <input name="email" placeholder='Department e.g:BsCs' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
-     <input name="email" placeholder='Section' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
+     <input name="department" placeholder='Department e.g:BsCs' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
+     <input name="section" placeholder='Section' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
          
       <input name="email" placeholder='Email Address' className='text-xs lg:text-base text-black border-black border-2 rounded-lg p-2 lg:w-[70%] w-[90%] focus:border-red-600 focus:border-2'/>
       <input name="password" type="password" placeholder='Password'  className='text-xs  lg:text-base text-black border-black border-2 rounded-lg p-2 w-[90%] lg:w-[70%]'/>
@@ -35,7 +55,7 @@ const Register = () => {
   <label className="text-xs lg:text-base text-black  mb-8">
   General
   </label>
-      <button className='bg-red-600 text-white lg:p-4 p-3 rounded-lg lg:w-[70%] w-[90%]'>Proceed</button>
+      <button className='bg-red-600 text-white lg:p-4 p-3 rounded-lg lg:w-[70%] w-[90%]' type="submit">Proceed</button>
       </Form>
    
     </div>
